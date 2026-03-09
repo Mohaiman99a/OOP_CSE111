2e9bb731ce1f491f080c5ed3772ff298f537d76b
Mohaiman99a <mohaiman99a@gmail.com>
2026-03-09 16:47:19 +0600

    Create task06.java

---
+++ A5_Linked List😒/task06.java
@@ -0,0 +1,174 @@
+//6
+
+public class Song{
+  public String title;
+  public String artist;
+  public int length;
+  public Song next;      //extra attribute *next*
+  
+   public Song(){
+  }
+  
+  
+  public Song(String title, String artist, int length){
+    this.title= title;
+    this.artist=artist;
+    this.length=length;
+    this.next=null;
+  }
+  
+  public void songInfo(){
+    System.out.println("Title: " + title);
+    System.out.println("Artist: " + artist);
+    System.out.println("Length: " + length + " minutes");
+  }
+  
+}
+
+
+
+
+
+public class Playlist{
+  public String p_name;
+  public Song head;
+  public int count=0;
+  
+  public Playlist(String p_name){
+    this.p_name=p_name;
+    System.out.println(p_name + " created.");
+  }
+  
+  
+  
+  //Add one song after the last one
+  public void addSong(Song s){
+    System.out.println(s.title + " added to " + p_name + ".");
+    
+    Song a=new Song();
+    a.title=s.title;
+    a.artist=s.artist;
+    a.length=s.length;
+    a.next=null;
+    count++;
+    
+    if(head==null){
+      head=a;
+    }
+    
+    else{
+      Song n=head;
+      while(n.next!=null){
+        n=n.next;
+      }
+      n.next=a;
+    }
+  }
+  
+  
+  
+  //Add song before the first
+  public void addAtStart(Song s){
+    Song a=new Song();
+    a.title=s.title;
+    a.artist=s.artist;
+    a.length=s.length;
+    a.next=head;
+    head=a;
+    count++;
+  }
+  
+  
+  
+  
+  
+  //Add song at any position
+  public void addSong(Song s, int index){
+    
+    if(index<=count){
+    System.out.println(s.title + " added to " + p_name + ".");
+    Song a=new Song();
+    a.title=s.title;
+    a.artist=s.artist;
+    a.length=s.length;
+    a.next=null;
+    
+    if(index==0){
+      addAtStart(s);
+    }
+    
+    else{
+    Song n=head;
+    for(int i=0; i<index-1; i++){
+      n=n.next;
+    }
+    a.next=n.next;
+    n.next=a;
+  }
+    }
+    
+    else{
+        System.out.println("Cannot add song to Index " + index+".");
+      }
+    }
+  
+  
+  
+  //Print
+  public void info(){
+    count=1;
+    System.out.println(p_name + " has the following songs: ");
+    
+    if(head!=null){
+      Song n=head;
+      while(n.next!=null){
+        System.out.println("Song-" + count++);
+        System.out.println("Title: " + n.title);
+        System.out.println("Artist: " + n.artist);
+        System.out.printf("Length: %d minutes\n", n.length);
+        n=n.next;
+      }
+      System.out.println("Song-" + count++);
+      System.out.println("Title: " + n.title);
+      System.out.println("Artist: " + n.artist);
+      System.out.printf("Length: %d minutes\n", n.length);
+    }
+    
+    else{
+      System.out.println("No songs in " + p_name + ".");
+    }
+  }
+
+  
+    //Play song
+  public void playSong(String a){
+    boolean found=false;
+    
+    Song n=head;
+    while(n.next!=null){
+      if((n.title).equals(a)){
+        found=true;
+        break;
+      }
+      
+      else{
+             n=n.next;
+      }
+    }
+    
+    //loop r bahire
+     if((n.title).equals(a)){
+        found=true;
+      }
+    
+     if(found==false){
+       System.out.println(a+" not found in playlist " + p_name+ ".");
+     }
+     
+     else{
+       System.out.println("Playing " +n.title + " by " + n.artist + ".");
+     }
+     
+  }
+}
+
