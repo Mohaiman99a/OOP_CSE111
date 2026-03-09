f8661fb576dac1d56e9d50fd6fb5e6e4c1863eab
Mohaiman99a <mohaiman99a@gmail.com>
2026-03-09 16:26:52 +0600

    Create task03.java

---
+++ A2_OOP/task03.java
@@ -0,0 +1,8 @@
+//3
+public class CSECourse{
+  //The value of instance variable are given from the start
+  public String courseName= "Programming Language II";
+  public String courseCode= "CSE111";
+  public int credit= 3;
+  
+}
