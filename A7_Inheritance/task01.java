3666ddd20077dcd09870872bc67ad5feaef9d87f
Mohaiman99a <mohaiman99a@gmail.com>
2026-03-09 16:55:55 +0600

    Create task01.java

---
+++ A7_Inheritance/task01.java
@@ -0,0 +1,16 @@
+//1
+public class BBAStudent extends Student{ 
+  
+  
+  public BBAStudent(){
+    super.setName("Default");
+    super.setDepartment("BBA");
+  }
+  
+    public BBAStudent(String s){
+    super.setName(s);
+    super.setDepartment("BBA");
+  } 
+  
+  
+}
