efcee980cfab8562daef98fa7696f63c721ddb3e
Mohaiman99a <mohaiman99a@gmail.com>
2026-03-09 16:44:48 +0600

    Create task01.java

---
+++ A5_Linked List😒/task01.java
@@ -0,0 +1,23 @@
+//1
+public class Song{
+  public String title;
+  public String artist;
+  public int length;
+  public Song next;      //extra attribute *next*
+  
+  
+  public Song(String title, String artist, int length){
+    this.title= title;
+    this.artist=artist;
+    this.length=length;
+    this.next=null;
+  }
+  
+  public void songInfo(){
+    System.out.println("Title: " + title);
+    System.out.println("Artist: " + artist);
+    System.out.println("Length: " + length + " minutes");
+  }
+  
+}
+
