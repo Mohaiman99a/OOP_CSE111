800360aafdd88779a8d35d89eea19d68bd379c1d
Mohaiman99a <mohaiman99a@gmail.com>
2026-03-09 16:27:14 +0600

    Create task04.java

---
+++ A2_OOP/task04.java
@@ -0,0 +1,11 @@
+//4
+   public class ImaginaryNumber{
+    public int realPart=0;
+    public int imaginaryPart=0;
+    
+    //Method to print
+    public void printNumber(){
+      System.out.println(realPart + "+" + imaginaryPart +"i");
+    }
+
+}
